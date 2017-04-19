36ce050a5840a9ba8ff975181035b84df5574f0e
Tomas <tomika619@gmail.com>
2017-04-19 20:37:55 +0200

    Program.cs comments added

--- Hashing/Program.cs
+++ Hashing/Program.cs
@@ -16,25 +16,27 @@ namespace Hashing
 {
     class Program
     {
+        //public List do ktoreho ulozime dvojice meno,heslo
         public static List<Tuple<string, string>> users = new List<Tuple<string, string>>();
 
+        /// <summary>
+        /// Funkcia nacita dvojice meno,heslo zo suboru a ulozi ich do listu
+        /// </summary>
         public static void nacitaj()
         {
-
-
             string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Tomáš Baka\Documents\Visual Studio 2015\Projects\Hashing\Hashing\users.txt");
             foreach (string s in lines)
             {
                 string[] tmp = s.Split(' ');
                 users.Add(new Tuple<string, string>(tmp[0], tmp[1]));
             }
-
         }
 
+        /// <summary>
+        /// Funkcia prejde cely list dvojic meno,heslo , zahashuje heslo pomocou MD5 bez saltu a posiela request na server kde sa ulozi hash do databazy
+        /// </summary>
         public static void naplnMD5S()
-        {
-
-           
+        {           
             foreach (Tuple<string, string> t in users)
             {
 
@@ -55,15 +57,14 @@ namespace Hashing
             }
 
         }
+        /// <summary>
+        /// Funkcia prejde cely list dvojic meno,heslo , zahashuje heslo pomocou Argon2 a posiela request na server kde sa ulozi hash do databazy
+        /// </summary>
         public static void naplnArgon()
-        {
-
-            
+        {            
             foreach (Tuple<string, string> t in users)
             {
-
-                string[] get = Argon2_hasher.GenerateHash(t.Item2, "");
-               
+                string[] get = Argon2_hasher.GenerateHash(t.Item2, "");              
 
                 JArray array = new JArray();
                 array.Add(t.Item1);
@@ -77,14 +78,15 @@ namespace Hashing
                 string json = o.ToString();
                 
                 ConnectionManager.SendData("https://147.175.98.36/insert.php", json);
-
             }
         }
 
-        public static void naplnMD5()
-        {
 
-          
+        /// <summary>
+        /// Funkcia prejde cely list dvojic meno,heslo , zahashuje heslo pomocou MD5 so saltom a posiela request na server kde sa ulozi hash do databazy
+        /// </summary>
+        public static void naplnMD5()
+        {          
             foreach (Tuple<string, string> t in users)
             {
 
@@ -104,13 +106,13 @@ namespace Hashing
                 ConnectionManager.SendData("https://147.175.98.36/insert.php",json);
 
             }
-
         }
 
+        /// <summary>
+        /// Funkcia prejde cely list dvojic meno,heslo , zahashuje heslo pomocou PBKDF2 a posiela request na server kde sa ulozi hash do databazy
+        /// </summary>
         static void naplnPBKDF()
-        {
-
-            
+        {            
             foreach (Tuple<string, string> t in users)
             {
 
@@ -129,9 +131,11 @@ namespace Hashing
                 //Console.WriteLine(json);
                 ConnectionManager.SendData("https://147.175.98.36/insert.php", json);
             }
-
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public static void md5Plain()
         {
             foreach (Tuple<string, string> t in users)
